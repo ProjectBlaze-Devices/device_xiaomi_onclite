29f8a66de540b48ffa6a1b887f26cef4367e1617
bibarub <bibarub@zohomail.eu>
2021-10-20 21:41:13 +0300

    onclite: build android.frameworks.displayservice@1.0.vendor

--- device.mk
+++ device.mk
@@ -123,6 +123,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.frameworks.displayservice@1.0.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     libgui_vendor \

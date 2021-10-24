6ce63f76b3f91dd0da86463dd3cf668ef1d9e4b4
SakilMondal <mondalsakil@gmail.com>
2021-10-24 07:08:03 +0000

    onclite: Fix recovery stuck issue
    Change-Id: I2a6e0577ba6e488334859e20569d932866449ab7

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -5783,3 +5783,7 @@ esac
 misc_link=$(ls -l /dev/block/bootdevice/by-name/misc)
 real_path=${misc_link##*>}
 setprop persist.vendor.mmi.misc_dev_path $real_path
+
+rm -rf /data/system/storage.xml
+touch /data/system/storage.xml
+chattr +i /data/system/storage.xml

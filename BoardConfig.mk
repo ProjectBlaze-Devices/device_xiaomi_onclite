dc771312a3939fa371008ecb70efe2a17cd3702c
ticklemykettle <ticklemykettleau@protonmail.com>
2021-12-31 02:11:34 +0600

    onclite: Include sepolicy/private

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -185,6 +185,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SELINUX_IGNORE_NEVERALLOWS := true
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # Treble
 BOARD_VNDK_VERSION := current

9b610cdbd30cf01b86deb6ce2bb629d39251b6ed
jacksproject <shahinboss888@gmail.com>
2022-01-28 11:57:11 +0530

    onclite: Add more apps in RemovePackages Debloat some useless apps Remove PrebuiltDeskClockGoogle from list

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := LiveWallpapersPicker CalendarGooglePrebuilt PrebuiltDeskClockGoogle arcore DiagnosticsToolPrebuilt PixelWallpapers2021 Photos talkback AndroidAutoStubPrebuilt PixelLiveWallpaperPrebuilt RecorderPrebuilt SafetyHubPrebuilt TipsPrebuilt
+LOCAL_OVERRIDES_PACKAGES := LiveWallpapersPicker CalendarGooglePrebuilt arcore DiagnosticsToolPrebuilt PixelWallpapers2021 Photos talkback AndroidAutoStubPrebuilt PixelLiveWallpaperPrebuilt RecorderPrebuilt SafetyHubPrebuilt TipsPrebuilt CompanionDeviceManager KeyChain NfcNci
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null

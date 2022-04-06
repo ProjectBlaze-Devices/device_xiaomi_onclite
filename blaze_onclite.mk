8bf7bdba9ec4059edbb37e1ba5a075f2e6ce8ac4
afterallafk <shivamatiet2001@gmail.com>
2022-04-06 19:10:04 +0000

    onclite: Raven april build/fp & priv desc
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- blaze_onclite.mk
+++ blaze_onclite.mk
@@ -30,9 +30,9 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 7
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "xiaomi/onc/onc:9/PKQ1.181021.001/V11.0.3.0.PFLMIXM:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:12/SP2A.220405.004/8233519:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="onc-user 9 PKQ1.181021.001 V11.0.3.0.PFLMIXM release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 SP2A.220405.004 8233519 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi

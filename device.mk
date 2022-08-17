56b79be86776d499c5b8aa43d2cc3e0b436ac94b
hpnightowl <iamhp2k@gmail.com>
2022-08-17 14:21:05 +0530

    onclite: Drop audio.a2dp.default * Building from here:   https://github.com/LineageOS/android_build/blob/lineage-18.1/target/product/full_base.mk#L35
    Change-Id: Ib71c1abd381468ec3621d60e5ca959f738f51168

--- device.mk
+++ device.mk
@@ -84,7 +84,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.service \
-    audio.a2dp.default \
     audio.primary.msm8953 \
     audio.r_submix.default \
     audio.usb.default \

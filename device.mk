fb6014bb41910c5473d2cead0bfc5f02a6110abb
TheSanty <sudhiryadav.igi@gmail.com>
2022-08-19 20:24:13 +0530

    onclite: Build QTI Perf Vendor Variant * F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/vendor.qti.hardware.perf@2.0-service": library "vendor.qti.hardware.perf@2.0.so" not found: needed by main executable
    Signed-off-by: TheSanty <sudhiryadav.igi@gmail.com>
    Change-Id: I62cfdc8836dc23facbd3eb9a6e46b7117cc12e67
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- device.mk
+++ device.mk
@@ -305,6 +305,13 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
+# Perf
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.perf@2.0.vendor \
+    vendor.qti.hardware.perf@2.1.vendor \
+    vendor.qti.hardware.perf@2.2.vendor \
+    libqti-perfd-client
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \

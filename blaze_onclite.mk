9864e5ebe8692c786477e49a43840a052acd9204
afterallafk <shivamatiet2001@gmail.com>
2022-09-13 18:57:39 +0530

    onclite: Update build fp to september 2022
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- blaze_onclite.mk
+++ blaze_onclite.mk
@@ -31,9 +31,9 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 7
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "google/raven/raven:12/SQ3A.220705.003/8671607:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:13/TP1A.220905.004/8927612:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="raven-user 12 SQ3A.220705.003 8671607 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 13 TP1A.220905.004 8927612 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi

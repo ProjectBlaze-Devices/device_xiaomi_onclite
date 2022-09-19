176c6d171600dc7fcf32d8ec2da88a860ace6240
KyuoFoxHuyu <88403766@qq.com>
2022-09-19 19:48:50 +0000

    onclite: build libavservices_minijail and vendor variant too CANNOT LINK EXECUTABLE "/system/vendor/bin/ims_rtp_daemon": library "libavservices_minijail.so" not found: needed by main executable
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- device.mk
+++ device.mk
@@ -290,6 +290,10 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.onclite
 
 # Media
+PRODUCT_PACKAGES += \
+    libavservices_minijail \
+    libavservices_minijail.vendor
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml::$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \

26cf756562005e6380725a8c89ae05aa3ce6cc94
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2022-10-01 22:02:20 +0530

    onclite: Build android.hardware.bluetooth.audio-impl
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- device.mk
+++ device.mk
@@ -114,6 +114,8 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor \
     audio.bluetooth.default \
     android.hardware.bluetooth.audio@2.1-impl \
+    android.hardware.bluetooth.audio-impl \
+    android.hardware.bluetooth.audio@2.0-impl \
     libbthost_if \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \

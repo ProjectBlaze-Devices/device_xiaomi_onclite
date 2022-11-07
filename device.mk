9ff575d6418b4baa6423193b6b9c7af729b2057f
David <davidmolinamorales1999@gmail.com>
2022-11-07 15:38:34 +0530

    onclite: Build android.hardware.graphics.allocator@3.0-service I hwservicemanager: getTransport: Cannot find entry android.hardware.graphics.allocator@3.0::IAllocator/default in either framework or device VINTF manifest. E LegacySupport: Could not get passthrough implementation for android.hardware.graphics.allocator@3.0::IAllocator/default.

--- device.mk
+++ device.mk
@@ -150,6 +150,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.allocator@3.0-impl \
+    android.hardware.graphics.allocator@3.0-service \
     android.hardware.graphics.allocator@4.0-impl \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service \

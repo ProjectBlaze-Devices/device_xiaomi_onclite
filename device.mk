30f4892f006fa56ba36c7d58fc348544f84b7082
henricaodopao <allricknation@gmail.com>
2022-11-08 17:00:09 +0600

    onclite: dont build QTI power hal It doesn't work
    * E QTI PowerHAL: Failed to perform launch boost
    * E QTI PowerHAL: Failed to acquire lock.

--- device.mk
+++ device.mk
@@ -329,7 +329,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
+    android.hardware.power-service-service \
     vendor.qti.hardware.perf@2.0.vendor \
     android.hardware.power@1.2.vendor
 

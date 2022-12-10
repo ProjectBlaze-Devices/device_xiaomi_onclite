96ea080118cada41b0e5459ce18f0be00b7d55b3
arco <arco68@gmail.com>
2022-12-10 04:50:21 +0000

    onclite: Silence clang warnings during kernel compile clang: warning: argument unused during compilation: '-fuse-ld=lld' [-Wunused-command-line-argument]

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -68,6 +68,8 @@ TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_VERSION := 4.9
 TARGET_KERNEL_CLANG_VERSION := proton
 TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-$(TARGET_KERNEL_CLANG_VERSION)
+TARGET_KERNEL_ADDITIONAL_FLAGS := \
+    HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
 # ANT
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"

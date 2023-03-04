f57f626419f921cdcf8a3ff5c9e2e59fcc5e95ac
Bruno Martins <bgcngm@gmail.com>
2023-03-05 00:19:50 +0530

    onclite: Explicitly build additional IMS stack dependency
    Commit 160fc65 removed the lib from PRODUCT_PACKAGES with the
    assumption that it was implicitely built while having
    TARGET_USES_QTI_CAMERA flag set. Turns out that build system
    skips compilation of vendor.qti.hardware.camera.device@1.0
    that is supposed to land in /system_ext/lib64 and this one is
    a required dependency of the current IMS stack.
    
    Change-Id: I7fdbdc1b837df670ecaf6775946784017c68134c

--- device.mk
+++ device.mk
@@ -131,7 +131,11 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0:64 \
+    android.hardware.camera.common@1.0 \
     android.frameworks.displayservice@1.0.vendor \
+    android.hardware.camera.provider@2.4 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     android.frameworks.sensorservice@1.0.vendor \

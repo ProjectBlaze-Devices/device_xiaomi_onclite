1240454b3d99a2ed25d0950ef4662d4b8b858dcb
Matsvei Niaverau <matvej2002@gmail.com>
2023-03-05 00:21:59 +0530

    onclite: Build additional camera libs
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- device.mk
+++ device.mk
@@ -135,10 +135,24 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0:64 \
     android.hardware.camera.common@1.0 \
     android.frameworks.displayservice@1.0.vendor \
+    android.frameworks.sensorservice@1.0.vendor \
     android.hardware.camera.provider@2.4 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    android.frameworks.sensorservice@1.0.vendor \
+    android.hardware.camera.provider@2.5 \
+    android.hardware.camera.provider@2.5.vendor \
+    android.hardware.camera.provider@2.6 \
+    android.hardware.camera.provider@2.6.vendor \
+    android.hardware.camera.device@3.2 \
+    android.hardware.camera.device@3.2.vendor \
+    android.hardware.camera.device@3.3 \
+    android.hardware.camera.device@3.3.vendor \
+    android.hardware.camera.device@3.4 \
+    android.hardware.camera.device@3.4.vendor \
+    android.hardware.camera.device@3.5 \
+    android.hardware.camera.device@3.5.vendor \
+    android.hardware.camera.device@3.6 \
+    android.hardware.camera.device@3.6.vendor \
     libgui_vendor \
     libstdc++ \
     libstdc++.vendor \

68a0c51ad87fb7682ed5834abe15b35f9f352ca2
Gabriel Biren <gbiren@google.com>
2023-10-10 21:35:22 +0530

    onclite: Use the AIDL WiFi service
    Bug: 205044134
    Test: Presubmit tests
    Change-Id: Icdaa448e2ed59b638056593adb4bcd769f34220d

--- device.mk
+++ device.mk
@@ -493,7 +493,7 @@ PRODUCT_COPY_FILES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     libcld80211 \
     libqsap_sdk \
     libQWiFiSoftApCfg \

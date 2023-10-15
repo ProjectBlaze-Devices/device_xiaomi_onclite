8fe12dbbf337e8a76c3d2d199a5e6f637e2feeb6
jacksproject <contact@jackbd.me>
2023-10-15 12:06:33 +0600

    onclite: Bringup for crDroid
    Signed-off-by: jacksproject <contact@jackbd.me>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/colt_onclite.mk
+    $(LOCAL_DIR)/lineage_onclite.mk
 
 COMMON_LUNCH_CHOICES := \
-    colt_onclite-user \
-    colt_onclite-userdebug \
-    colt_onclite-eng
+    lineage_onclite-user \
+    lineage_onclite-userdebug \
+    lineage_onclite-eng
--- lineage_onclite.mk
+++ lineage_onclite.mk
@@ -12,11 +12,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from onclite device
 $(call inherit-product, device/xiaomi/onclite/device.mk)
 
-# Inherit some common ColtOS stuff.
-$(call inherit-product, vendor/colt/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := colt_onclite
+PRODUCT_NAME := lineage_onclite
 PRODUCT_DEVICE := onclite
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 7
@@ -32,11 +32,8 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 # Define bootanimation resolution.
 TARGET_BOOT_ANIMATION_RES := 720
 
-# Build Gapps variant.
-WITH_GAPPS := true
-
 # Face Unlock
 TARGET_FACE_UNLOCK_SUPPORTED := true
 
-# Define maintainer name.
-COLT_BUILD_MAINTAINER := "JacksProject™"
+# Blurs
+TARGET_ENABLE_BLUR := true

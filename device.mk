0e2e8d129d0f05059c6caee581ef22a795d371d3
Albert I <kras@raphielgang.org>
2023-11-21 12:30:15 +0000

    onclite: Inherit several Android Go configurations Beneficial for everyone, even on non-Go devices.
    Change-Id: I088fd00500ce10df5f413cb762aaed2ec00f5dfc
    Signed-off-by: jacksproject <contact@jackbd.me>

--- device.mk
+++ device.mk
@@ -246,6 +246,10 @@ PRODUCT_PACKAGES += \
     ims-ext-common \
     ims_ext_common.xml
 
+# Inherit several Android Go Configurations (Beneficial for everyone, even on non-Go devices)
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/idc/uinput-fpc.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-fpc.idc \

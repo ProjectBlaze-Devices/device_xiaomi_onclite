3842727b490d09b59806f9d1874f89294feb43a7
afterallafk <shivamatiet2001@gmail.com>
2023-12-22 23:48:16 +0530

    onclite: Update vendorsetup.sh
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,24 +1,18 @@
 # Remove existing HALs
-rm -rf hardware/qcom-caf/wlan
 rm -rf hardware/qcom-caf/msm8996/audio
-rm -rf hardware/qcom-caf/msm8996/media
-rm -rf hardware/qcom-caf/msm8996/display
+#rm -rf hardware/qcom-caf/msm8996/media
+#rm -rf hardware/qcom-caf/msm8996/display
 
 # Clone HALs
-git clone https://github.com/MSM8953-Tree/hardware_qcom-caf_msm8996_audio -b 13 hardware/qcom-caf/msm8996/audio
-git clone https://github.com/MSM8953-Tree/hardware_qcom-caf_msm8996_media -b 12 hardware/qcom-caf/msm8996/media
-git clone https://github.com/MSM8953-Tree/hardware_qcom-caf_msm8996_display -b 13 hardware/qcom-caf/msm8996/display
-git clone https://github.com/MSM8953-Tree/android_hardware_qcom-caf_wlan -b 12 hardware/qcom-caf/wlan
+git clone https://github.com/afterallafk/hardware_qcom-caf_msm8996_audio.git -b 14 hardware/qcom-caf/msm8996/audio
+#git clone https://github.com/afterallafk/hardware_qcom-caf_msm8996_media.git -b 14 hardware/qcom-caf/msm8996/media
+#git clone https://github.com/afterallafk/hardware_qcom-caf_msm8996_display.git -b 14 hardware/qcom-caf/msm8996/display
 
 # Clone Proton-clang
 git clone --depth=1 https://github.com/kdrag0n/proton-clang.git -b master prebuilts/clang/host/linux-x86/clang-proton
 
 # Clone Kernel
-git clone https://github.com/MSM8953-Tree/android_kernel_xiaomi_onclite -b 13 kernel/xiaomi/onclite
+git clone --depth=1 https://github.com/afterallafk/kernel_xiaomi_onclite-dynamic -b dynamic kernel/xiaomi/onclite
 
 # Clone Vendor
 git clone https://github.com/MSM8953-Tree/android_vendor_xiaomi_onclite -b 13 vendor/xiaomi/onclite
-
-# Define Host & username
-export BUILD_HOSTNAME=JackPC
-export BUILD_USERNAME=jack

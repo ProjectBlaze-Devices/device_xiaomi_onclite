7ee7fdd844ec375b828135454068c94017a38d6b
afterallafk <shivamatiet2001@gmail.com>
2023-12-26 15:36:02 +0530

    onclite: Clone hals
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,12 +1,12 @@
 # Remove existing HALs
 rm -rf hardware/qcom-caf/msm8996/audio
-#rm -rf hardware/qcom-caf/msm8996/media
-#rm -rf hardware/qcom-caf/msm8996/display
+rm -rf hardware/qcom-caf/msm8996/media
+rm -rf hardware/qcom-caf/msm8996/display
 
 # Clone HALs
 git clone https://github.com/afterallafk/hardware_qcom-caf_msm8996_audio.git -b 14 hardware/qcom-caf/msm8996/audio
-#git clone https://github.com/afterallafk/hardware_qcom-caf_msm8996_media.git -b 14 hardware/qcom-caf/msm8996/media
-#git clone https://github.com/afterallafk/hardware_qcom-caf_msm8996_display.git -b 14 hardware/qcom-caf/msm8996/display
+git clone https://github.com/afterallafk/hardware_qcom-caf_msm8996_media.git -b 14 hardware/qcom-caf/msm8996/media
+git clone https://github.com/afterallafk/hardware_qcom-caf_msm8996_display.git -b 14 hardware/qcom-caf/msm8996/display
 
 # Clone Proton-clang
 git clone --depth=1 https://github.com/kdrag0n/proton-clang.git -b master prebuilts/clang/host/linux-x86/clang-proton

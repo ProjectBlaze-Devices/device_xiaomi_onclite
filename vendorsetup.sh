7387cb4727f53acc355a1559186bf74b70b89779
afterallafk <shivamatiet2001@gmail.com>
2024-02-28 23:58:01 +0530

    onclite: Track patched sepolicy
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,7 +9,9 @@ rm -rf hardware/qcom-caf/msm8996/audio
 rm -rf hardware/qcom-caf/msm8996/media
 rm -rf hardware/qcom-caf/msm8996/display
 rm -rf hardware/qcom-caf/common
+rm -rf system/sepolicy
 git clone https://github.com/afterallafk/hardware_qcom-caf_msm8996_audio.git -b 14 hardware/qcom-caf/msm8996/audio
 git clone https://github.com/afterallafk/hardware_qcom-caf_msm8996_media.git -b 14 hardware/qcom-caf/msm8996/media
 git clone https://github.com/afterallafk/hardware_qcom-caf_msm8996_display.git -b 14 hardware/qcom-caf/msm8996/display
 git clone https://github.com/afterallafk/hardware_qcom-caf_common.git -b lineage-21.0 hardware/qcom-caf/common
+git clone https://github.com/afterallafk/system_sepolicy.git -b 14.1 system/sepolicy
